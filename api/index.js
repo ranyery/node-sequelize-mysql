6b685c25f85a9b8adc61ac6675c6da8949a8103d
ranyery <ranyerysc@gmail.com>
2022-08-11 15:00:10 -0300

    init: adiciona base do projeto

---
+++ api/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(express.json());
+
+app.get("/ping", (req, res) => {
+  res.send({ message: "Pong!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}: http://localhost:${PORT}`);
+});
